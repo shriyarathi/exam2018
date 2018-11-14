e6733c69ee5c9b663abf62181d69bc6da12bb78b
shriyarathi <42847638+shriyarathi@users.noreply.github.com>
2018-11-14 00:13:02 -0500

    Add files via upload

--- exam_p2.py
+++ exam_p2.py
@@ -1,3 +1,6 @@
+import csv
+
+
 def process_file(file_name):
     """
     Given a file name, returns a list of lists [name, gender, births]
@@ -12,17 +15,31 @@ def process_file(file_name):
     [["Mary","F",7065], ["Anna","F",2604],...]
 
     """
-    pass  # delete this line and replace with your code here
+    with open(file_name, 'r') as f:
+        reader = csv.reader(f)
+        your_list = list(reader)
 
-
+        return (your_list)
+  
+    
 def total_births(year):
     """
 
     :param year: an integer, between 1880 and 2010
     :return: an integer, the total births of all the babies in that year
     """
-    pass  # delete this line and replace with your code here
 
+    file_name = 'exam2018-master/babynames/yob' + str(year) + '.txt'
+
+    nameslist = process_file(file_name) 
+
+    totalbirths = 0
+
+    for name in nameslist:
+        totalbirths += int(name[2])
+
+    return totalbirths
+            
 
 def proportion(name, gender, year):
     """
@@ -32,8 +49,15 @@ def proportion(name, gender, year):
     :param year: an integer, between 1880 and 2010
     :return: a floating number, the proportion of babies with the given name to total births in given year
     """
-    pass  # delete this line and replace with your code here
+    totalb = total_births(year) 
 
+    file_name = 'exam2018-master/babynames/yob' + str(year) + '.txt'
+
+    nameslist = process_file(file_name) 
+
+    for namegenbirth in nameslist:
+        if namegenbirth[0] == name and namegenbirth[1] == gender:
+            return float(namegenbirth[2])/float(totalb)
 
 def highest_year(name, gender):
     """
@@ -42,11 +66,27 @@ def highest_year(name, gender):
     :param gender: a string, "F" or "M"
     :return: an integer, the year when the given name has the highest proportion over the years (among all the proportions of the same name in different years)
     """
-    pass  # delete this line and replace with your code here
+    maxprop = float(0) 
+    maxyear = 0
+
+    for year in range(1880, 2010):
+        prop = proportion(name, gender, year)
+
+        if prop > maxprop:
+            prop = maxprop
+            maxyear = year 
+
+    return year 
+
 
 
 def main():
-    pass  # delete this line and replace with your code here
+    #print(process_file('exam2018-master/babynames/yob1880.txt'))
+    print(total_births(1981))
+    print(proportion('Sarah', 'F', 1981))
+    print(highest_year('Sarah', 'F'))
+    
+
 
 
 if __name__ == '__main__':
